4404cc01ef59a0fcf7c2e162dc15e0e4cbec1567
JongMany <blackberry1114@naver.com>
2023-10-04 20:04:36 +0900

    feat/#1.simple setting

---
+++ src/api/httpInterface.ts
@@ -0,0 +1,9 @@
+import { ICommunication } from './http';
+import { Response } from 'http-proxy-middleware/dist/types';
+
+export class HttpInterface {
+  constructor(private apiClient: ICommunication) {}
+
+  // TODO: Api 명세 대로 구현하기
+
+}
