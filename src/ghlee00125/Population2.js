c4d448ffc23efcff96a1b3a922b21ce7ae49f8ed
GeonHeeLeee <ghlee00125@naver.com>
2023-12-01 13:36:06 +0900

    0,65세 시각화 색상 변경 및 검은색 배경에 맞춤

--- src/ghlee00125/Population2.js
+++ src/ghlee00125/Population2.js
@@ -24,14 +24,21 @@ function PopulationChart() {
         data: filteredData,
         showSymbol: false,
         name: `Age ${age}`,
-        animation: true, // 애니메이션 효과를 추가합니다.
+        animation: true,
+        lineStyle: {
+          color: age === 65 ? "#EF9034" : "#D4D4D4", // 나이가 65인 경우 'EF9034'로, 0인 경우 '#D4D4D4'로 선 색을 설정
+        }, // 애니메이션 효과를 추가합니다.
       };
     });
 
     const option = {
+      backgroundColor: "black", // 배경색을 검은색으로 설정
       animationDuration: 10000,
       title: {
         text: "Population of Age 0 and 65 since 1970",
+        textStyle: {
+          color: "white", // 제목의 색상을 하얀색으로 설정
+        },
       },
       tooltip: {
         trigger: "axis",
@@ -40,9 +47,19 @@ function PopulationChart() {
         type: "category", // 'category' 타입을 사용합니다.
         nameLocation: "middle",
         interval: 1, // 5년 단위로 눈금을 표시합니다.
+        axisLine: {
+          lineStyle: {
+            color: "white", // x축의 색상을 하얀색으로 설정
+          },
+        },
       },
       yAxis: {
         name: "Population",
+        axisLine: {
+          lineStyle: {
+            color: "white", // y축의 색상을 하얀색으로 설정
+          },
+        },
       },
       grid: {
         right: 140,
