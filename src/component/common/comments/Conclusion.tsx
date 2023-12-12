c529e17a1fec4dc751f0b3932852e97212cb8aa6
kosssshhhh <kosukwhon@gmail.com>
2023-12-12 15:17:13 +0900

    최종 수정

--- src/component/common/comments/Conclusion.tsx
+++ src/component/common/comments/Conclusion.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import useObserver from '../../../hooks/useObserver';
-import { opacityVariants, opacityVariants2 } from '../../../@constants/animation/animation';
+import {
+  opacityVariants,
+  opacityVariants2,
+} from '../../../@constants/animation/animation';
 import styles from './Conclusion.module.css';
 import styles2 from './Highlight.module.css';
 
@@ -14,12 +17,12 @@ export default function Conclusion() {
       initial='hidden'
       animate={animation}
       variants={opacityVariants2}
-      className={styles.section}
-    >
+      className={styles.section}>
       <div>
         <div className={styles.contents}>
           <p className={styles.headline}>
             '한국, 흑사병 때보다 인구 감소가 빨라'
+            <br />
             <br />
             '지구상에서 제일 먼저 사라지는 국가는 한국일 것이다'
           </p>
